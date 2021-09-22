3f9fdbae83e1d67b77e5e3420bec56d6d0e4a395
alanafilgueiras <75276526+alanafilgueiras@users.noreply.github.com>
2021-09-21 22:46:31 -0300

    Delete banco.php

--- includes/banco.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$banco = new mysqli ("localhost", "root", "123456", "db_games");
-	if($banco->connect_errno) {
-		echo "<p>Encontrei um erro $banco->errno --> $banco->connect_error</p>";
-		die();
-	}
-	

618f52dfd1c6679b27e15cab666415dcb044a726
alana <alana.filgueiras@gmail.com>
2021-09-25 09:22:38 -0300

    alteracao de dados no banco

--- user-edit.php
+++ user-edit.php
@@ -2,7 +2,7 @@
 <?php
 	require_once "includes/banco.php";
 	require_once "includes/login.php";
-	require_once  "includes/funcoes.php";
+	require_once "includes/funcoes.php";
 ?>
 <html lang="pt-br">
     <head>
@@ -22,12 +22,37 @@
                 if(!isset($_POST['usuario'])){
                     include "user-edit-form.php";
                 }else{
-                    echo msg_sucesso("dados foram passados");
+                    $usuario = $_POST['usuario'] ?? null;
+                    $nome  = $_POST['nome'] ?? null;
+                    $tipo = $_POST['tipo'] ?? null;
+                    $senha1 = $_POST['senha1'] ?? null; 
+                    $senha2 = $_POST['senha2'] ?? null;
+
+                    $q= "update usuarios set usuario = '$usuario', nome = '$nome'";
+                    
+                    if (empty($senha1) || is_null ($senha1)){ 
+                        echo msg_aviso("Senha foi mantida.");
+
+                    }else {
+                        if ($senha1 === $senha2){
+                            $senha = gerarHash($senha1);
+                            $q .= ", senha='$senha'";                        
+                        }else{
+                            echo msg_aviso("Senhas não conferem. A senha anterior será mantida.");
+                        }
+                    }
+                    $q .= " where usuario = '". $_SESSION ['user'] . "'";
+                    if ($banco->query($q)){
+                        echo msg_sucesso("Usuário alterado com sucesso");
+                        logout();
+                        echo msg_aviso("Por segurança, efetue o <a href='user-login.php'>login"); 
+                    }else{
+                        echo msg_erro("Não foi possivel alterar os dados.");
+                    }
                 }
-                echo voltar();
             }
         ?>
-
+        <?php echo voltar(); ?>
     </div>
     
         <?php
